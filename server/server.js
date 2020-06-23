2de4f3d22b696450334a9af6332776feb0e9cf19
mattchupp <mattlchupp@gmail.com>
2020-06-23 11:28:54 -0500

    added some connected and disconnected messages

--- server/server.js
+++ server/server.js
@@ -3,18 +3,27 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http); 
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello world!</h1>');
+  // Load index.html template
   res.sendFile(__dirname + '/index.html'); 
 }); 
 
+// set up chat connection
 io.on('connection', (socket) => {
   console.log('User connected'); 
+  // const name = prompt('What is your name?'); 
+  const name = "User"
+  io.emit('chat message', `${name} connected`); 
+
+  // do this one chat message
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg); 
     io.emit('chat message', msg); 
   }); 
 
+  // when a user disconnects
   socket.on('disconnect', () => {
+    // io.broadcast('User disconnected')
+    io.emit('chat message', 'User disconnected')
     console.log('disconnected'); 
   })
 })
