6faaf9b8c470199eb15e710f3a0b0909487bc407
mattchupp <mattlchupp@gmail.com>
2020-06-23 11:19:36 -0500

    setting up socket chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import SocketIoClient from 'socket.io-client'
 
 
 function Chat() {
 
+  useEffect(() => {
+
+    const socket = SocketIoClient("/"); 
+
+
+  }, [])
+
   return (
     <div>
+      <ul id="messages">
+        
+      </ul>
 
+      <form action="">
+        <input id="m" autocomplete="off" /><button>Send</button>
+      </form>
     </div>
   )
 
