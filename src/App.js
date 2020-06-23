18b8eba364800bf05077dfba29a25844cb04eabe
mattchupp <mattlchupp@gmail.com>
2020-06-23 11:13:58 -0500

    set up chat component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chat from './components/Chat'
 
 function App() {
   return (
     <div className="App">
-      
+      <Chat />
     </div>
   );
 }
---
+++ src/components/Chat.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+
+function Chat() {
+
+  return (
+    <div>
+
+    </div>
+  )
+
+}
+
+export default Chat
